8e4ceacc745b725f8c76676aa2e053a1f6721152
Aniket Mishra <mishra2@ualberta.ca>
2023-11-21 16:48:46 -0700

    Update

--- loss.py
+++ loss.py
@@ -1,3 +1,13 @@
+# calculate the loss of the model
+# we calculate it using the cross entropy loss
+# Basically we just take log of the softmax output and then take the negative of it
+
+# # Answer is 2
+# try_array = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+# print(len(try_array.shape))
+# # Answer is 1
+# try_array = np.array([1, 2, 3, 4, 5])
+# print(len(try_array.shape))
 class Loss:
     def __init__(self):
         
---
+++ try.py
@@ -0,0 +1,7 @@
+import numpy as np
+# Answer is 2
+try_array = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+print(len(try_array.shape))
+# Answer is 1
+try_array = np.array([1, 2, 3, 4, 5])
+print(len(try_array.shape))
