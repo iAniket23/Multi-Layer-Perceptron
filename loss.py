2dff86eed1e268a4243ff94322506911e36f9d8e
Aniket Mishra <mishra2@ualberta.ca>
2023-11-28 21:40:14 -0700

    Update loss.py

--- loss.py
+++ loss.py
@@ -1,3 +1,4 @@
+import numpy as np
 # calculate the loss of the model
 # we calculate it using the cross entropy loss
 # Basically we just take log of the softmax output and then take the negative of it
@@ -9,8 +10,16 @@
 # try_array = np.array([1, 2, 3, 4, 5])
 # print(len(try_array.shape))
 class Loss:
-    def __init__(self, output, target):
-        self.output = output
-        self.target = target
-        self.loss = self.cross_entropy_loss()
+    def calculate(self, output, target):
+        # calculate the loss
+        sample_losses = self.forward(output, target)
+
+        # calculate the mean loss
+        data_loss = np.mean(sample_losses)
+        
+        return data_loss
+    
+class Loss_CategoricalCrossEntropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
         
